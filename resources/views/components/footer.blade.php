458babd322becf48f46c598b5ed03af19f35218f
mahnev_sa <mahnev_sa@irgups.ru>
2023-05-15 07:55:34 +0800

    #50 footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <p class="mb-0">&copy; {{ date('Y') }} Ваш проект. Все права защищены.</p>
+                <p class="mb-0">&copy; {{ date('Y') }} Сервис подарков. Все права защищены.</p>
             </div>
         </div>
     </div>
