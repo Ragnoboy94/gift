daba683efeede7d9a49e5d7df8a86d4acf2be733
mahnev_sa <mahnev_sa@irgups.ru>
2023-08-08 12:23:22 +0800

    фикс перевода

--- resources/lang/en/new.php
+++ resources/lang/en/new.php
@@ -10,4 +10,5 @@ return [
     'error1'=>'Your level does not allow you to take more orders.',
     'problem'=>'Payment problem',
     'problem_text'=>'If the order was collected, paid for by you and handed over to the customer without a corresponding payment, you should contact the nearest law enforcement department with payment receipts. When contacting us from law enforcement agencies, this service will provide all available information about the client. Also, please let us know about the problem and we will take action towards the customer.',
-];
+    'problem_text 1'=>'The delivered goods must be confirmed by a receipt and have a marketable appearance. If the product does not meet expectations, this is not a reason for refusing the order and does not exempt from the obligation to pay. In case of non-fulfillment of payment obligations, the contractor has the right to apply to law enforcement agencies. Please be careful and responsible when placing orders.',
+    ];
